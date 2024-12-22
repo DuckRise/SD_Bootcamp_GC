f48f1b0e3f885cdd22ab3f633bfc157632a06e40
DuckRise <DuckRise>
2024-12-22 02:21:38 +0000

    JUnit - Correct initial starting balance in MainTest.java

--- BankingApp/src/test/java/MainTest.java
+++ BankingApp/src/test/java/MainTest.java
@@ -7,7 +7,7 @@ public class MainTest {
     @Test
     public void testAddAccount() {
         Main atm = new Main();
-        BankAccount account = new SmallBusinessAccount("Small Business Account", "1", 1);
+        BankAccount account = new SmallBusinessAccount("Small Business Account", "1", 0);
         atm.addAccount(account);
         assertSame(atm.findAccount("1"), account);
     }
@@ -15,7 +15,7 @@ public class MainTest {
     @Test
     public void testDeposit() {
         Main atm = new Main();
-        BankAccount account = new SmallBusinessAccount("Small Business Account", "1", 1);
+        BankAccount account = new SmallBusinessAccount("Small Business Account", "1", 0);
         atm.addAccount(account);
         assertTrue(atm.deposit("1", 100.0));
         assertEquals(100.0, atm.checkBalance("1"), 0.001);
@@ -24,7 +24,7 @@ public class MainTest {
     @Test
     public void testCheckBalance() {
         Main atm = new Main();
-        BankAccount account = new SmallBusinessAccount("Small Business Account", "1", 1);
+        BankAccount account = new SmallBusinessAccount("Small Business Account", "1", 0);
         atm.addAccount(account);
         assertEquals(0.0, atm.checkBalance("1"), 0.001);
         atm.deposit("1", 100.0);
@@ -35,8 +35,8 @@ public class MainTest {
     public void testFindAccount() {
         Main atm = new Main();
         BankAccount account1 =
-                new SmallBusinessAccount("Small Business Account", "1", 1);
-        BankAccount account2 = new CommunityAccount("Community Account", "2", 2);
+                new SmallBusinessAccount("Small Business Account", "1", 0);
+        BankAccount account2 = new CommunityAccount("Community Account", "2", 0);
         atm.addAccount(account1);
         atm.addAccount(account2);
         assertSame(atm.findAccount("1"), account1);
